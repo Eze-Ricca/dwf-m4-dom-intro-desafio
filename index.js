5cb080896e496c03c655fd42aa2c0a8a9f0bf2fe
Eze <Riccaesteban@gmail.com>
2023-10-11 23:13:22 -0300

    subiendo contenido de vsc

--- index.js
+++ index.js
@@ -25,6 +25,30 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEl = document.querySelector(".lista");
+  const itemsEls = document.querySelectorAll(".lista li");
+  // console.log(listaEl);
+  // listaEl.forEach((el) => console.log(el));
+  setTimeout(() => {
+    for (let el of itemsEls) {
+      el.remove();
+    }
+  }, 3000);
+  setTimeout(() => {
+    for (const item of cosasQueAprendimos) {
+      // console.log(item);
+      const newLiel = document.createElement("li");
+      // console.log(newLiel);
+      newLiel.textContent = item.tema;
+      // console.log(newLiel);
+      const laClaseQueLeVoyAAsignarAlli = item.class || "item";
+      // console.log(laClaseQueLeVoyAAsignarAlli);
+      newLiel.classList.add(laClaseQueLeVoyAAsignarAlli);
+      // console.log(newLiel);
+      listaEl.appendChild(newLiel);
+    }
+  }, 4000);
+}
 
 main();
